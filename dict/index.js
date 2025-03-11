362c7ac178a4b65cba19d8f138bb845a62ea8d7e
Ra1nningDay <123659453+Ra1nningDay@users.noreply.github.com>
2025-03-11 20:47:48 +0700

    complie index.ts to js

--- dict/index.js
+++ dict/index.js
@@ -1,15 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const app = express();
-const port = process.env.PORT;
-
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Hello");
 });
-
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
